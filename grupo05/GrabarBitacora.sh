ca79f271c7cc814e5c2a4e05f4af43009be4ecf2
Juan Manuel Bouvier <juanmanuel.bouvier@adoos.com>
2016-04-06 02:56:11 -0300

    Primer commit. Script de GrabarBitacora ya funcionando. Configurar variables de ambiente para ejecutarlo desde el prompt (chequear modo de uso en carpeta de Google Drive).

---
+++ grupo05/GrabarBitacora.sh
@@ -0,0 +1,32 @@
+COMANDO=$1	# Comando, nombre del archivo de log
+
+MSG=$2	# Mensaje a loggear
+
+if [ -z ${3+x} ]; then		# El tercer argumento es el tipo de mensaje
+	MSG_TYPE="INFO";	# INFO es tipo por default
+else
+	if [ $3 -eq 2 ]; then		# Tipo 2 es ERROR
+		MSG_TYPE="ERR"
+	elif [ $3 -eq 1 ]; then		# Tipo 1 es WARNING
+		MSG_TYPE="WAR"
+	else				# Tipo 0 es INFORMATIVO
+		MSG_TYPE="INFO"
+	fi
+fi
+
+DATE=`date +%d/%m/%Y" "%H:%M:%S`	# Fecha actual
+
+# Escribo registro de log al final del archivo
+# Si el archivo de bitacora no existe, creo uno nuevo
+BITACORA="$LOGDIR$COMANDO.log"
+echo "$USER $DATE $COMANDO [$MSG_TYPE]: $MSG" >> $BITACORA
+
+# Si el tamanio del archivo supera LOGSIZE se trunca a ultimas 50 lineas
+CURRENT_LOGSIZE="$(wc -c < $BITACORA)"
+if [ $((CURRENT_LOGSIZE)) -gt $((LOGSIZE)) ]; then
+	LAST_REGS=`tail -n 50 $BITACORA`
+	rm $BITACORA
+	echo "$LAST_REGS" > $BITACORA
+	echo "$USER $DATE $COMANDO [INFO]: Log Excedido" >> $BITACORA
+fi
+
