c86cad8521ba1876efd153e1b61591be16d7950f
martin <martincura@gmail.com>
2016-04-26 02:27:24 -0300

    RecibirOfertas: Arreglos tras inspeccion

--- grupo05/RecibirOfertas.sh
+++ grupo05/RecibirOfertas.sh
@@ -1,6 +1,8 @@
 #####De prueba
 ARRIDIR="ARRIDIR/"
 MAEDIR="MAEDIR/"
+NOKDIR="NOKDIR/"
+OKDIR="OKDIR/"
 SLEEPTIME=10 #segundos
 #####
 
@@ -37,15 +39,14 @@ obtenerFechaUltAdjudicacion() {
 
 # Rechazo de archivo moviendolo a $NOKDIR y loggeo de explicacion
 rechazarArchivo() {
-	nom_arch_rechazado=$1
-	razon_rechazo=$2
+	nom_arch_rechazado="$1"
+	razon_rechazo="$2"
 
 	sh MoverArchivo.sh "$ARRIDIR$nom_arch_rechazado" "$NOKDIR$nom_arch_rechazado"
 	RES_MOV=$?
 	### Verificar "$?" igual 0?, hablar con buby
 	sh GrabarBitacora.sh "RecibirOfertas.sh" "Archivo $ARRIDIR$nom_arch_rechazado rechazado y movido a $NOKDIR$nom_arch_rechazado, por $razon_rechazo"
 }
-
 
 
 nro_ciclo=0
@@ -58,21 +59,21 @@ ARCH_FECHAS_ADJ="$MAEDIR""FechasAdj.csv"
 while :
 do
 	# Incremento e imprimo el número de ciclo
-	((nro_ciclo+=1))  			###Si no funciona en sh usar nro_ciclo=$((nro_ciclo+1))
+	((nro_ciclo+=1))
 	sh GrabarBitacora.sh "$0" "ciclo nro. $nro_ciclo"
 
 	# Obtengo una lista de todos los archivos en la carpeta $ARRIDIR
-	find $ARRIDIR -maxdepth 1 -type f > RecibirOfertasAUX.aux
+	find "$ARRIDIR" -maxdepth 1 -type f > RecibirOfertasAUX.aux
 
 	# Por cada archivo encontrado...
-	while read -r linea
+	while read -r linea_arch
 	do
-		# Chequeo que el archivo sea de tipo text
-		text=$(file $linea | grep "text")
+		# Chequeo que el tipo archivo sea del archivo mencione ´text´
+		text=$(file "$linea_arch" | grep "text")
 		# Extraigo nombre de archivo sin directorios
-		nom_arch=$(echo $linea | sed 's-.*/\([^/]*\)$-\1-g')
+		nom_arch=$(echo "$linea_arch" | sed 's-.*/\([^/]*\)$-\1-g')
 
-		# Verifico estas 2 condiciones
+		# Verifico que el archivo sea de texto y el formato del nombre correcto
 		if [ -n "$text" -a validarFormatoNombreArchivo "$nom_arch" ]
 		  then
 			fecha_arch=${nom_arch:5:8}	# Formato YYYYMMDD
@@ -80,26 +81,24 @@ do
 
 			# Obtengo fecha de ultima adjudicacion y fecha de hoy para comparaciones
 			fecha_ult_adj=0
-			obtenerFechaUltAdjudicacion
+			obtenerFechaUltAdjudicacion # modifica $fecha_ult_adj
 			fecha_hoy=$(date +%Y%m%d)
 
 			# Hago los chequeos de fecha valida, menor o igual a hoy, y mayor a la de adj. obtenida
 			if [ -n "$fecha_valida" -a $fecha_arch -gt $fecha_ult_adj -a $fecha_hoy -ge $fecha_arch ]
 			  then
-				### Chequeo de si existe el archivo $ARCH_CONCESIONARIOS????????
 				# Chequeo existencia del concesionario
-				linea_concesionario=$(cat "$ARCH_CONCESIONARIOS" | grep ";${nom_arch:0:4}")
-				if [ -n $linea_concesionario ]
+				linea_concesionario=$(cat "$ARCH_CONCESIONARIOS" | grep ";${nom_arch:0:4}$")
+				if [ -n "$linea_concesionario" ]
 				  then
 					### Verificar funcionamiento de esto
 					# Chequeo tamanio del archivo mayor a 0
-					if [ -s "$linea" ]
+					if [ $(wc -c < "$linea_arch") -gt 1 ] # semialt.: [ -s "$linea_arch" ]
 					  then
-						### ACEPTADO: mover a $OKDIR y loggear nombre y path
-						sh MoverArchivo.sh "$linea" "$OKDIR$nom_arch"
-						RES_MOV="$?"
-						### Verificar "$?" como 0, hablar con buby
-						sh GrabarBitacora.sh "$0" "Archivo $linea aceptado y movido a $OKDIR$nom_arch"
+						sh MoverArchivo.sh "$linea_arch" "$OKDIR$nom_arch"
+						RES_MOV=$?
+						### Verificar "$?" igual a 0?, hablar con buby
+						sh GrabarBitacora.sh "$0" "Archivo $linea_arch aceptado y movido a $OKDIR$nom_arch"
 					else
 						rechazarArchivo "$nom_arch" "encontrarse vacio"
 					fi
@@ -110,15 +109,23 @@ do
 				rechazarArchivo "$nom_arch" "fecha invalida o anterior a la ultima adjudicacion"
 			fi
 		else
-			rechazarArchivo "$nom_arch" "tipo o nombre del archivo"
+			rechazarArchivo "$nom_arch" "invalido tipo o nombre del archivo"
 		fi
 	done < RecibirOfertasAUX.aux
 	rm RecibirOfertasAUX.aux
 
-	if [ "$(ls -A '$OKDIR')" ]
+		#####cdp
+	if [ false ] #### "$(ls -A "$OKDIR")" ]	### DOBLES QUOTES: puede fallar, en cuyo caso buscar otra forma de hacer este chequeo
 	  then # hay archivos aceptados en $OKDIR para procesar
 		## Lanza ProcesarOfertas sin ningun parametro
 		sh LanzarProceso.sh "sh ProcesarOfertas.sh" "$0"
+		if [ $? -eq 0 ] ### Chequear con buby
+		  then
+			sh GrabarBitacora.sh "$0" "ProcesarOfertas corriendo bajo el no.: " ###NECESITO EL PID DE P.O.
+		elif [ $? -eq 1 ]
+			sh GrabarBitacora.sh "$0" "B" ####
+		else
+			sh GrabarBitacora.sh "$0" "C" ####
 	fi
 
 	sleep $SLEEPTIME
