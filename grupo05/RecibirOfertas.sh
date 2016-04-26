eb88ac6220bf71124a6b8b40b08582dcef4c7cdf
martin <martincura@gmail.com>
2016-04-26 03:52:51 -0300

    RecibirOfertas: Testeado al 90 %, cuando el resto termine su parte podré verificar lo que queda.

--- grupo05/RecibirOfertas.sh
+++ grupo05/RecibirOfertas.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 #####De prueba
 ARRIDIR="ARRIDIR/"
 MAEDIR="MAEDIR/"
@@ -9,7 +11,7 @@ SLEEPTIME=10 #segundos
 validarFormatoNombreArchivo() {
 	# Formato buscado: <cod_concesionaria_de_4_digitos>_<aniomesdia>.csv
 	aux=$(echo $1 | grep '^\([0-9]\{4,4\}_[12][0-9]\{3,3\}[0-1][0-9][0-3][0-9]\.[cC][sS][vV]\)$')
-	if [ "$aux" -eq "$1" ]
+	if [ "$aux" = "$1" ]
 	  then
 		echo 0
 	else
@@ -26,12 +28,13 @@ obtenerFechaUltAdjudicacion() {
 		if [ -n "$ult_adj" ]
 		  then
 			# Extraigo la fecha y reformateo a YYYYMMDD
-			fecha_ult_adj=${ult_adj:6:4}${ult_adj:3:2}${ult_adj:0:2}
+			fecha_ult_adj=$(echo "$ult_adj" | cut -c7-10)$(echo "$ult_adj" | cut -c4-5)$(echo "$ult_adj" | cut -c1-2)
 			return 0
 		fi
 	fi
 	#####cdp
 	sh GrabarBitacora.sh "$0" "COME FIND ME, I NEED YOU OBI WAN KENOBI" '1'
+	echo "YOURE MY ONLY HOPE"
 	#####
 	fecha_ult_adj=0
 	return 1
@@ -49,17 +52,17 @@ rechazarArchivo() {
 }
 
 
-nro_ciclo=0
+# Los archivos de novedades llegan a $ARRIDIR en la forma <cod_concesionario>_<aniomesdia>.csv
 ARCH_CONCESIONARIOS="$MAEDIR""concesionarios.csv"
 ARCH_FECHAS_ADJ="$MAEDIR""FechasAdj.csv"
-# Archivos de novedades llegan en la forma $ARRIDIR<cod_concesionario>_<aniomesdia>.csv
 
+nro_ciclo=0
 
 # daemon
 while :
 do
 	# Incremento e imprimo el número de ciclo
-	((nro_ciclo+=1))
+	nro_ciclo=$((nro_ciclo+1))
 	sh GrabarBitacora.sh "$0" "ciclo nro. $nro_ciclo"
 
 	# Obtengo una lista de todos los archivos en la carpeta $ARRIDIR
@@ -74,26 +77,25 @@ do
 		nom_arch=$(echo "$linea_arch" | sed 's-.*/\([^/]*\)$-\1-g')
 
 		# Verifico que el archivo sea de texto y el formato del nombre correcto
-		if [ -n "$text" -a validarFormatoNombreArchivo "$nom_arch" ]
+		if [ -n "$text" -a $(validarFormatoNombreArchivo "$nom_arch") -eq 0 ]
 		  then
-			fecha_arch=${nom_arch:5:8}	# Formato YYYYMMDD
+			fecha_arch=$(echo $nom_arch | cut -c6-13)  # Formato YYYYMMDD
 			fecha_valida=$(date -d "$fecha_arch")
 
 			# Obtengo fecha de ultima adjudicacion y fecha de hoy para comparaciones
 			fecha_ult_adj=0
-			obtenerFechaUltAdjudicacion # modifica $fecha_ult_adj
+			obtenerFechaUltAdjudicacion  # modifica $fecha_ult_adj
 			fecha_hoy=$(date +%Y%m%d)
 
 			# Hago los chequeos de fecha valida, menor o igual a hoy, y mayor a la de adj. obtenida
 			if [ -n "$fecha_valida" -a $fecha_arch -gt $fecha_ult_adj -a $fecha_hoy -ge $fecha_arch ]
 			  then
 				# Chequeo existencia del concesionario
-				linea_concesionario=$(cat "$ARCH_CONCESIONARIOS" | grep ";${nom_arch:0:4}$")
+				linea_concesionario=$(cat "$ARCH_CONCESIONARIOS" | grep ";$(echo $nom_arch | cut -c1-4)")
 				if [ -n "$linea_concesionario" ]
 				  then
-					### Verificar funcionamiento de esto
 					# Chequeo tamanio del archivo mayor a 0
-					if [ $(wc -c < "$linea_arch") -gt 1 ] # semialt.: [ -s "$linea_arch" ]
+					if [ $(wc -c < "$linea_arch") -gt 1 ]   # semialternativa: [ -s "$linea_arch" ]
 					  then
 						sh MoverArchivo.sh "$linea_arch" "$OKDIR$nom_arch"
 						RES_MOV=$?
@@ -114,8 +116,7 @@ do
 	done < RecibirOfertasAUX.aux
 	rm RecibirOfertasAUX.aux
 
-		#####cdp
-	if [ false ] #### "$(ls -A "$OKDIR")" ]	### DOBLES QUOTES: puede fallar, en cuyo caso buscar otra forma de hacer este chequeo
+	if [ "$(ls -A "$OKDIR")" ]	### DOBLES QUOTES: pueden fallar, en cuyo caso buscar otra forma de hacer este chequeo
 	  then # hay archivos aceptados en $OKDIR para procesar
 		## Lanza ProcesarOfertas sin ningun parametro
 		sh LanzarProceso.sh "sh ProcesarOfertas.sh" "$0"
